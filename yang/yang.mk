3947feafec46a945eb8e20f008bf0540b7868030
Med <mohamed.boucadair@orange.com>
2024-05-24 11:14:36 +0200

    Update yang.mk

--- yang/yang.mk
+++ yang/yang.mk
@@ -20,7 +20,8 @@ TXT=$(patsubst $(YANGDIR)/%.yang,%-diagram.txt,$(YANG))
 .PHONY: yang-lint yang-gen-diagram yang-clean
 
 pyang-lint: $(STDYANG) $(STDYANGDIR)
-	pyang -V --ietf -f tree --tree-line-length=70 --tree-print-groupings -p $(YANG_PATH) $(STDYANG)	
+	pyang -V --ietf -f tree --tree-line-length=70 --tree-print-groupings -p $(YANG_PATH) $(STDYANG)
+        pyang –f yang --yang-canonical --yang-line-length 69 -p $(YANG_PATH) $(STDYANG)
 
 yang-lint: $(STDYANG) $(STDYANGDIR)
 	yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules -p $(STDYANGDIR)/standard/ieee/published/802.1/ $(STDYANG) -i
